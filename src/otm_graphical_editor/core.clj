ff6a34c12d572c130dace7dc0c34d25fee30f87c
Marc Sabates Gelpi <marc.sabates.gelpi@protonmail.com>
2018-03-18 16:49:22 +0000

    Refactor

--- src/otm_graphical_editor/core.clj
+++ src/otm_graphical_editor/core.clj
@@ -4,38 +4,39 @@
             [otm-graphical-editor.image :as i])
   (:gen-class))
 
-(def parse-while-xf (comp
-                        (map #(s/split % #" "))
-                        (map (fn [[cmd & args]]
-                               (vector (keyword (s/lower-case cmd))
-                                       (map string->char-or-num args))))
-                        (halt-when #(= % :x))))
-
 (defn- string->char-or-num
-  "Returns char or number"
-  [x]
-  (let [xx (read-string x)]
-    (if (number? xx) xx (first (seq x)))))
+  "Returns char or number."
+  [s]
+  (let [parsed (read-string s)]
+    (if (number? parsed) parsed (first (seq s)))))
+
+(def ^:private parse-while-xf (comp
+                               (map #(s/split % #" "))
+                               (map (fn [[cmd & args]]
+                                      (vector
+                                       (keyword (s/lower-case cmd))
+                                       (map string->char-or-num args))))
+                               (halt-when #{:x})))
 
 (defmulti run-cmd
-  "Execute image commands"
+  "Execute image commands."
   (fn [_ cmd _] cmd))
 
 (defmethod run-cmd :i
   ^{:doc "I M N​
-   Create a new M x N image with all pixels coloured white (O)"}
+   Create a new M x N image with all pixels coloured white (O)."}
   [_ _ [cols rows]]
   (i/make-image cols rows i/colour-white))
 
 (defmethod run-cmd :c
   ^{:doc "C​
-   Clears the table, setting all pixels to white (O)"}
+   Clears the table, setting all pixels to white (O)."}
   [image & _]
   (i/make-image (i/get-num-cols image) (i/get-num-rows image) i/colour-white))
 
 (defmethod run-cmd :l
   ^{:doc "L X Y C​
-   Colours the pixel (X,Y) with colour C"}
+   Colours the pixel (X,Y) with colour C."}
   [image _ [col row colour]]
   (let [pixel [col row]]
     (if (i/get-pixel image pixel)
@@ -45,43 +46,43 @@
 (defmethod run-cmd :v
   ^{:doc "V X Y1 Y2 C​
    Draw a vertical segment of colour C in column X between rows Y1 and 
-   Y2 (inclusive)"}
+   Y2 (inclusive)."}
   [image _ [col r1 r2 colour]]
-  (let [pixels (for [r (range r1 (inc r2))] [col r])
+  (let [pixels (for [r (range r1 (inc r2))] (vector col r))
         colour (or colour i/colour-white)]
     (if (and
-         (i/get-pixel image [col r1])
-         (i/get-pixel image [col r2]))
+         (i/get-pixel image (vector col r1))
+         (i/get-pixel image (vector col r2)))
       (reduce (fn [image pixel] (i/set-pixel image pixel colour)) image pixels)
       image)))
 
 (defmethod run-cmd :h
   ^{:doc "H X1 X2 Y C​
    Draw a horizontal segment of colour C in row Y between columns X1 and
-   X2 (inclusive)"}
+   X2 (inclusive)."}
   [image _ [c1 c2 row colour]]
-  (let [pixels (for [c (range c1 (inc c2))] [c row])
+  (let [pixels (for [c (range c1 (inc c2))] (vector c row))
         colour (or colour i/colour-white)]
     (if (and
-         (i/get-pixel image [c1 row])
-         (i/get-pixel image [c2 row]))
+         (i/get-pixel image (vector c1 row))
+         (i/get-pixel image (vector c2 row)))
       (reduce (fn [image pixel] (i/set-pixel image pixel colour)) image pixels)
       image)))
 
 (defn- adjacents
-  "Gets a set of valid adjacent pixel coordinates"
+  "Gets a set of valid adjacent pixel coordinates."
   [image [c r]]
   (->> (into
-        (for [cc '(1 -1)] [(+ c cc) r])
-        (for [rr '(1 -1)] [c (+ r rr)]))
+        (for [cc '(1 -1)] (vector (+ c cc) r))
+        (for [rr '(1 -1)] (vector c (+ r rr))))
        (filter #(when (i/get-pixel image %) %))
        set))
  
 (defn- find-region
-  "Gets the set of pixel's coordinates for the pixel's region"
+  "Gets the set of pixel's coordinates for the pixel's region."
   [image pixel]
   (let [colour (i/get-pixel image pixel)]
-    (loop [region #{} visited #{} to-visit #{pixel}]
+    (loop [region #{} visited #{} to-visit (hash-set pixel)]
       (if (seq to-visit)
         region
         (let [current (first to-visit)]
@@ -98,36 +99,38 @@
   ^{:doc "F X Y C​
    Fill the region R with the colour C. R is defined as: Pixel (X,Y)
    belongs to R. Any other pixel which is the same colour as (X,Y) and
-   shares a common side with any pixel in R also belongs to this region"}
+   shares a common side with any pixel in R also belongs to this region."}
   [image _ [col row colour]]
   (let [pixel [col row]]
-    (reduce (fn [image pixel] (i/set-pixel image pixel colour)) image (find-region image pixel))))
+    (reduce (fn [image pixel] (i/set-pixel image pixel colour))
+            image
+            (find-region image pixel))))
 
 (defmethod run-cmd :s
   ^{:doc "S​
-   Show the contents of the current image"}
+   Show the contents of the current image."}
   [image & _]
   (doseq [x (i/get-rows image)] (println (apply str x)))
   image)
 
 (defmethod run-cmd :x
   ^{:doc "X​
-   Terminate the session"}
+   Terminate the session."}
   [image & _]
   image)
 
 (defn- run-interactive
-  "Runs the editor from the standard input"
+  "Runs the editor from the standard input."
   []
   (loop [image (i/make-empty-image) line (read-line)]
     (let [[cmd & args] (s/split line #" ")
           cmd (keyword (s/lower-case cmd))
           args (map string->char-or-num args)]
-      (if (not= :x cmd)
+      (when-not (#{:x} cmd)
         (recur (run-cmd image cmd args) (read-line))))))
 
 (defn- process-batch
-  "Parses the input lines and execute them"
+  "Parses the input lines and execute them."
   [coll]
   (transduce parse-while-xf
              (completing (fn [image [cmd args]]
@@ -136,7 +139,7 @@
              coll))
 
 (defn- run-from-file
-  "Runs the editor in batch mode"
+  "Runs the editor in batch mode."
   [file]
   (-> file
       slurp
@@ -144,7 +147,7 @@
       process-batch))
 
 (defn -main
-  "Runs the graphical editor"
+  "Runs the graphical editor."
   [& args]
   (if (seq args)
     (run-from-file (first args))
--- src/otm_graphical_editor/image.clj
+++ src/otm_graphical_editor/image.clj
@@ -1,36 +1,37 @@
 (ns otm-graphical-editor.image
   (:gen-class))
 
-;; Image implementation with a vector of vectors
-;; Each vector in the main vector is a row
-;; Each element in the subvector is a pixel
-;; Each pixel is a char representing a colour
-;; Each pixel position is identified by
-;; [col row] int values starting from 1
-;; All coordinates are swapped and decremented
-;;
-;; ex: [[\A \O \O]
-;;      [\B \O \O]
-;;      [\O \O \C]]
-;; pixel   internal pixel   colour
-;; [1 1]   [0 0]            \A
-;; [1 2]   [1 0]            \B
-;; [3 3]   [2 2]            \C
+;;;; Image implementation with a vector of vectors.
+
+;;; Each vector in the main vector is a row.
+;;; Each element in the subvector is a pixel.
+;;; Each pixel is a char representing a colour.
+;;; Each pixel position is identified by.
+;;; [col row] int values starting from 1.
+;;; All coordinates are swapped and decremented.
+;;;
+;;; ex: [[\A \O \O]
+;;;      [\B \O \O]
+;;;      [\O \O \C]]
+;;; pixel   internal pixel   colour
+;;; [1 1]   [0 0]            \A
+;;; [1 2]   [1 0]            \B
+;;; [3 3]   [2 2]            \C
 
 (def ^:const colour-white \O)
 
 (defn- pixel->internal-pixel
-  "Converts pixel coordinates"
+  "Converts pixel coordinates."
   [[col row]]
-  [(dec row) (dec col)])
+  (vector (dec row) (dec col)))
 
 (defn make-empty-image
-  "Makes and empty image"
+  "Makes and empty image."
   []
-  (vector))
+  [])
 
 (defn make-image
-  "Makes a cols x rows image with the especified colour"
+  "Makes a cols x rows image with the especified colour."
   ([cols rows]
    (make-image cols rows colour-white))
   ([cols rows colour]
@@ -38,27 +39,27 @@
      (vec (repeat rows r)))))
 
 (defn set-pixel
-  "Sets the colour of a pixel"
+  "Sets the colour of a pixel."
   [image pixel colour]
   (assoc-in image (pixel->internal-pixel pixel) colour))
 
 (defn get-pixel
   "Gets the colour of pixel.
-   Returns nil if the pixel doesn't exist"
+   Returns nil if the pixel doesn't exist."
   [image pixel]
   (get-in image (pixel->internal-pixel pixel)))
 
 (defn get-num-cols
-  "Gets num cols"
-  [[a-row & others]]
+  "Gets num cols."
+  [[a-row & _]]
   (count a-row))
 
 (defn get-num-rows
-  "Gets num rows"
+  "Gets num rows."
   [image]
   (count image))
 
 (defn get-rows
-  "Gets a collection of all the rows"
+  "Gets a collection of all the rows."
   [image]
   (seq image))
